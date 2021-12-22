929bd5a3a142f0a6608ea80a56598e69bce2bf44
fangyong <3422652607@qq.com>
2021-12-23 00:11:43 +0800

    更新

--- dubbodemo_consumer/src/main/java/com/atguigu/Git.java
+++ dubbodemo_consumer/src/main/java/com/atguigu/Git.java
@@ -3,5 +3,9 @@ package com.atguigu;
 public class Git {
     public static void main(String[] args) {
         System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
+        System.out.println(111111);
     }
 }
